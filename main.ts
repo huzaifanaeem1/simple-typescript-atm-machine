77a6dbcaa5a05e3334e763cc4a79a143899195eb
Huzaifa Naeem <159990985+huzaifanaeem1@users.noreply.github.com>
2024-03-27 05:34:07 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,64 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let myBalance = 20000; //Dollar $
+let myPin = 3351;
+
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your pin?",
+        type: "number"
+    }
+]);
+
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code!!!");
+
+    let operationAnswer = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Please select options",
+            type: "list",
+            choices: ["withdrawal", "check balance", "fast cash"]
+        }
+    ]);
+
+    if (operationAnswer.operation === "withdrawal") {
+        let amountAnswer = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter your amount",
+                type: "number"
+            }
+        ]);
+
+        if (amountAnswer.amount > myBalance) {
+            console.log("Insufficient balance! You cannot withdraw more than your current balance.");
+        } else {
+            myBalance -= amountAnswer.amount;
+            console.log(`Your remaining balance is: ${myBalance}`);
+        }
+    } else if (operationAnswer.operation === "check balance") {
+        console.log(`Your balance is: ${myBalance}`);
+
+    } else if (operationAnswer.operation === "fast cash") {
+        let fastCashAnswer = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Select your fast cash amount",
+                type: "list",
+                choices: [1000, 2000, 5000, 10000, 15000, 20000]
+            }
+        ]);
+
+        if (fastCashAnswer.amount > myBalance) {
+            console.log("Insufficient balance! You cannot withdraw more than your current balance.");
+        } else {
+            myBalance -= fastCashAnswer.amount;
+            console.log(`Your remaining balance is: ${myBalance}`);
+        }
+    }
+} else {
+    console.log("Incorrect pin code!!!");
+}
